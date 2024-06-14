643faa096d87a481ffd6582d7bdb16ccc5631262
Chris Arndt <christopher.arndt@wisc.edu>
2024-06-14 08:40:10 -0500

    Add scheduled tasks

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,19 @@
  */
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+	async scheduled(
+		event: ScheduledController,
+		env: Env,
+		ctx: ExecutionContext
+	) {
+		switch (event.cron) {
+		// You can set up to three schedules maximum.
+		case "*/15 * * * *":
+			console.log("This will be executed every 15 minutes");
+			break;
+		case "*/30 * * * *":
+			console.log("This will be executed every 30 minutes");
+			break;
+		}
+	  }
+	}
