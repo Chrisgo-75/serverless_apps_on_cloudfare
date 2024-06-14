9cb550807c4ad02f759663108601389cd8a5c1d9
Chris Arndt <christopher.arndt@wisc.edu>
2024-06-14 09:09:26 -0500

    Edited index.ts to be as it was. Created new file to represent scheduled cron jobs.

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,16 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export interface Env {
+	// MY_KV_NAMESPACE: KVNamespace;
+  }
+  
 export default {
-	async scheduled(
-		event: ScheduledController,
+	async fetch(
+		request: Request,
 		env: Env,
 		ctx: ExecutionContext
-	) {
-		switch (event.cron) {
-		// You can set up to three schedules maximum.
-		case "*/15 * * * *":
-			console.log("This will be executed every 15 minutes");
-			break;
-		case "*/30 * * * *":
-			console.log("This will be executed every 30 minutes");
-			break;
-		}
-	  }
-	}
+	): Promise<Response> {
+		return new Response('Hello World!');
+	},
+};
---
+++ src/scheduled.ts
@@ -0,0 +1,17 @@
+export default {
+	async scheduled(
+		event: ScheduledController,
+		env: Env,
+		ctx: ExecutionContext
+	) {
+		switch (event.cron) {
+		// You can set up to three schedules maximum.
+		case "*/15 * * * *":
+			console.log("This will be executed every 15 minutes");
+			break;
+		case "*/30 * * * *":
+			console.log("This will be executed every 30 minutes");
+			break;
+		}
+	  }
+	}
